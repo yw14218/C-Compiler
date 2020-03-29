80181443f8ad923e07123fdc3a2ac05c3ec3d0c1
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-29 06:24:45 +0100

    Add files via upload

--- include/operator/operator.hpp
+++ include/operator/operator.hpp
@@ -3,157 +3,167 @@
 
 #include"Statement.hpp"
 
-class timesoperator : public Statement{
+class Operator : public Statement{
+public:
+	virtual ~Operator(){};
+	Operator(Statementptr p1, Statementptr p2){left = p1 ; right = p2; }
+  virtual void translate(std::ostream &dst)const override{}
+protected:
+	Statementptr left;
+	Statementprt right;
+};
+
+class timesoperator : public Operator{
 public:
 	virtual ~timesoperator(){};
-	timesoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "*" << subs[1];}
+	timesoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+  void translate(std::ostream &dst){ dst << left << "*" << right;}
 };
 
 
-class divideoperator : public Statement{
+class divideoperator : public Operator{
 public:
 	virtual ~divideoperator(){};
-	divideoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "/" << subs[1];}
+	divideoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+  void translate(std::ostream &dst){ dst << left << "/" << right;}
 };
 
-class percentageoperator : public Statement{
+class percentageoperator : public Operator{
 public:
 	virtual ~percentageoperator(){};
-	percentageoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	percentageoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
 
-class plusoperator : public Statement{
+class plusoperator : public Operator{
 public:
 	virtual ~plusoperator(){};
-	plusoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "+" << subs[1];}
+	plusoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+  void translate(std::ostream &dst){ dst << left << "+" << right;}
 };
 
-class minusoperator : public Statement{
+class minusoperator : public Operator{
 public:
 	virtual ~minusoperator(){};
-	minusoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "-" << subs[1];}
+	minusoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+        void translate(std::ostream &dst){ dst << left << "-" << right;}
 };
-class lshiftoperator : public Statement{
+class lshiftoperator : public Operator{
 public:
 	virtual ~lshiftoperator(){};
-	lshiftoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	lshiftoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class rshiftoperator : public Statement{
+class rshiftoperator : public Operator{
 public:
 	virtual ~rshiftoperator(){};
-	rshiftoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	rshiftoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class smalleroperator : public Statement{
+class smalleroperator : public Operator{
 public:
 	virtual ~smalleroperator(){};
-	smalleroperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "<" << subs[1];}
+	smalleroperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+        void translate(std::ostream &dst){ dst << left << "<" << right;}
 };
 
-class biggeroperator : public Statement{
+class biggeroperator : public Operator{
 public:
 	virtual ~biggeroperator(){};
-	biggeroperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	biggeroperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
 
-class sequaloperator : public Statement{
+class sequaloperator : public Operator{
 public:
 	virtual ~sequaloperator(){};
-	sequaloperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	sequaloperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
 
-class bequaloperator : public Statement{
+class bequaloperator : public Operator{
 public:
 	virtual ~bequaloperator(){};
-	bequaloperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "==" << subs[1];}
+	bequaloperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+        void translate(std::ostream &dst){ dst << left << "==" << right;}
 };
-class equalequaloperator : public Statement{
+class equalequaloperator : public Operator{
 public:
 	virtual ~equalequaloperator(){};
-	equalequaloperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	equalequaloperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class notequaloperator : public Statement{
+class notequaloperator : public Operator{
 public:
 	virtual ~notequaloperator(){};
-	notequaloperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	notequaloperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class bitwiseandoperator : public Statement{
+class bitwiseandoperator : public Operator{
 public:
 	virtual ~bitwiseandoperator(){};
-	bitwiseandoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	bitwiseandoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class logicalandoperator : public Statement{
+class logicalandoperator : public Operator{
 public:
 	virtual ~logicalandoperator(){};
-	logicalandoperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "and" << subs[1];}
+	logicalandoperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+        void translate(std::ostream &dst){ dst << left << " and " << right;}
 };
-class logicaloroperator : public Statement{
+class logicaloroperator : public Operator{
 public:
 	virtual ~logicaloroperator(){};
-	logicaloroperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "or" << subs[1];}
+	logicaloroperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+        void translate(std::ostream &dst){ dst << left << " or " << right;}
 };
-class xoroperator : public Statement{
+class xoroperator : public Operator{
 public:
 	virtual ~xoroperator(){};
-	xoroperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	xoroperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 };
-class oroperator : public Statement{
+class oroperator : public Operator{
 public:
 	virtual ~oroperator(){};
-	oroperator(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
-        void translate(std::ostream &dst){ dst << subs[0] << "*" << subs[1];}
+	oroperator(Statementptr p1, Statementptr p2){left = p1 ; right = p2;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+  void translate(std::ostream &dst){ dst << left << "*" << right;}
 };
-class conditionaloperator : public Statement{
+/*class conditionaloperator : public Operator{
 public:
 	virtual ~conditionaloperator(){};
-	conditionaloperator(Statementptr p1, Statementptr p2, Statementptr &p3){subs[0] = p1 ; subs[1] = p2; subs[2] = p3;}
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	conditionaloperator(Statementptr p1, Statementptr p2, Statementptr &p3){left = p1 ; right = p2; subs[2] = p3;}
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
 	Statementptr get_stat3(){return subs[2];}
-};
+};*/
 
 
 #endif

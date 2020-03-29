9bd37e0d40b223da1a4e0d920f3013fd5125bc02
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-29 06:24:09 +0100

    Add files via upload

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -4,12 +4,8 @@ int main(int argc ,char *argv[])
 {
 
     const Statement *ast=parseAST();
-   	std::cout<< "START " << std::endl;	
+   	std::cout<< "START " << std::endl;
 	freopen (argv[2],"r",stdin);
-
-    const Ast_Node *ast=parseAST();
-	std::cout<< std::endl;	
-	
 	freopen (argv[4],"w",stdout);
     if(argv[1] == "--translate"){
         ast -> translate(std::cout);

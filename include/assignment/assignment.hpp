e352586c036a673b8286238fd1eeb6f62e24d6f4
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-30 00:55:00 +0100

    Add files via upload

--- include/assignment/assignment.hpp
+++ include/assignment/assignment.hpp
@@ -2,89 +2,131 @@
 #define ASSIGNMENT_HPP
 
 #include"Statement.hpp"
-
 class equalassignment : public Statement{
 public:
 	virtual ~equalassignment(){};
-	equalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
-	void translate(std::ostream &dst){ dst << subs[0] << "=" << subs[1];}
+	equalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual void translate(std::ostream &dst){ dst << left << "=" << right;}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class timesequalassignment : public Statement{
 public:
 	virtual ~timesequalassignment(){};
-	timesequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	timesequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class divideequalassignment : public Statement{
 public:
 	virtual ~divideequalassignment(){};
-	divideequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	divideequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class percentageequalassignment : public Statement{
 public:
 	virtual ~percentageequalassignment(){};
-	percentageequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	percentageequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class plusequalassignment : public Statement{
 public:
 	virtual ~plusequalassignment(){};
-	plusequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	plusequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class minusequalassignment : public Statement{
 public:
 	virtual ~minusequalassignment(){};
-	minusequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	minusequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 
 class lshiftequalassignment : public Statement{
 public:
 	virtual ~lshiftequalassignment(){};
-	lshiftequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	lshiftequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 class rshiftequalassignment : public Statement{
 public:
 	virtual ~rshiftequalassignment(){};
-	rshiftequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	rshiftequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 class andequalassignment : public Statement{
 public:
 	virtual ~andequalassignment(){};
-	andequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];}
-	Statementptr get_stat2(){return subs[1];}
+	andequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 class xorequalassignment : public Statement{
 public:
 	virtual ~xorequalassignment(){};
-	xorequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	xorequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 class orequalassignment : public Statement{
 public:
 	virtual ~orequalassignment(){};
-	orequalassignment(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;};
-	Statementptr get_stat1(){return subs[0];};
-	Statementptr get_stat2(){return subs[1];};
+	orequalassignment(Statementptr p1, Statementptr p2){left = p1 ; right = p2;};
+	Statementptr get_stat1(){return left;}
+	Statementptr get_stat2(){return right;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr left;
+	StatementPtr right;
 };
 #endif

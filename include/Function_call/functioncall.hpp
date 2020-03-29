c73f8018d10e80efa7191411b86ba4226fd55af7
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-30 00:57:56 +0100

    Add files via upload

--- include/Function_call/functioncall.hpp
+++ include/Function_call/functioncall.hpp
@@ -2,14 +2,19 @@
 #define FUNCTIONCALL_HPP
 
 #include"Statement.hpp"
+#include"list.hpp"
 
 class functioncall : public Statement{
 public:
 	virtual ~functioncall(){};
-	functioncall(Statementptr p1, Statementptr p2){subs[0] = p1 ; subs[1] = p2;}
-	Statementptr get_id(){return subs[0];}
-	Statementptr get_argument(){return subs[1];}
-	void translate(std::ostream& dst){dst << subs[0] << "(" << subs[1] << ")";}
+	functioncall(Statementptr p1, statementlistptr p2){left = p1 ; right = p2;}
+	Statementptr get_id(){return left;}
+	Statementptr get_argument(){return right;}
+	virtual void translate(std::ostream& dst){dst << left << "(" ;right -> translate(std::cout);dst << ")";}
+private:
+	Statementptr left;
+	Statementlistptr right;
+
 };
 
 #endif

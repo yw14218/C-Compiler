9341c7a4d6282668d109fd05bd78908d56735437
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-30 00:55:28 +0100

    Add files via upload

--- include/Unary/unary.hpp
+++ include/Unary/unary.hpp
@@ -6,86 +6,122 @@
 class unaryplusplus : public Statement{
 public:
 	virtual ~unaryplusplus(){};
-	unaryplusplus(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	unaryplusplus(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 
 class unaryminusminus : public Statement{
 public:
 	virtual ~unaryminusminus(){};
-	unaryminusminus(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	unaryminusminus(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class plusplusunary : public Statement{
 public:
 	virtual ~plusplusunary(){};
-	plusplusunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	plusplusunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class minusminusunary : public Statement{
 public:
 	virtual ~minusminusunary(){};
-	minusminusunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	minusminusunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class plusunary : public Statement{
 public:
 	virtual ~plusunary(){};
-	plusunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	plusunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class minusunary : public Statement{
 public:
 	virtual ~minusunary(){};
-	minusunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	minusunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class bitwiseandunary : public Statement{
 public:
 	virtual ~bitwiseandunary(){};
-	bitwiseandunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	bitwiseandunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class timesunary : public Statement{
 public:
 	virtual ~timesunary(){};
-	timesunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	timesunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class invertunary : public Statement{
 public:
 	virtual ~invertunary(){};
-	invertunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	invertunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class exclamunary : public Statement{
 public:
 	virtual ~exclamunary(){};
-	exclamunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	exclamunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class sizeofunary : public Statement{
 public:
 	virtual ~sizeofunary(){};
-	sizeofunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	sizeofunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 class sizeofnameunary : public Statement{
 public:
 	virtual ~sizeofnameunary(){};
-	sizeofnameunary(Statementptr unaryin){subs[0] =unaryin;}
-	Statementptr get_unary(){return subs[0];};
+	sizeofnameunary(Statementptr unaryin){target =unaryin;}
+	Statementptr get_unary(){return target;}
+	virtual virtual void translate(std::ostream &dst)const override{}
+private:
+	Statementptr target;
 };
 
 #endif

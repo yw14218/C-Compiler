e10875230c24febeac43c53dcee94167ad2c608d
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-30 22:29:58 +0100

    Update floating.hpp

--- include/primary_expression/floating.hpp
+++ include/primary_expression/floating.hpp
@@ -8,7 +8,7 @@ public:
 	virtual ~floating(){};
 	floating(float id_in){id = id_in;}
 	float getid(){return id;}
-	virtual void translate(std::ostream &dst)const override{}
+	virtual void translate(std::ostream &dst)const override{dst << id;}
 	virtual void treeprint(std::ostream &dst)const override {dst<<"<float> "<<id;}
 protected:
 	float id;

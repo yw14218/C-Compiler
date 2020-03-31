8910a73b290ca214bace42a8a98f48069577d9cc
Yilong <56489051+yw14218@users.noreply.github.com>
2020-03-31 04:44:04 +0100

    Add files via upload

--- include/list/list.hpp
+++ include/list/list.hpp
@@ -7,20 +7,19 @@
 class list : public Statement {
 public:
 	list(Statementlistptr p1){statementlistptr = p1;};
-	virtual ~list(){};
+	virtual ~list(){delete statementlistptr;}
 	Statementlistptr get_list(){return statementlistptr;}
-	virtual void translate(std::ostream &dst)const override{}
-/*	virtual virtual void translate(std::ostream &dst)const override{
+  virtual void translate(std::ostream &dst)const override{
 		int content = statementlistptr -> size();
 		if(content == 0){return;}
 		if(content == 1){dst << statementlistptr ->at(0);}
 		else{
 			for(int i =0; i<content-1;i++){
-         dst << at(i) << ",";
+         dst << statementlistptr ->at(i) << ",";
 			}
-			dst << at(content-1);
+			dst << statementlistptr ->at(content-1);
 		}
-	}*/
+	}
 	virtual void treeprint(std::ostream &dst)const override {
 		dst<<"<list>"<<" ["<<'\n';
 		for(int i=0;i<statementlistptr->size();i++)

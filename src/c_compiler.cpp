0ebaa2365d9399e349cfd5402c59652c281a4e03
Yilong <56489051+yw14218@users.noreply.github.com>
2020-04-01 04:18:01 +0100

    Add files via upload

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -1,27 +1,26 @@
 #include "ast.hpp"
 extern FILE *yyin;
 
-int main(int argc ,char *argv[])
-{
+int main(int argc ,char *argv[]){
 	std::cout<< "START " << std::endl;	
 	yyin = freopen (argv[2],"r",stdin);
     	Statementptr ast=parseAST();
    	std::cout<< "Parse finished " << std::endl;
 	std::string indent = "";
-//    if(argv[1] == "--translate"){
-  //      ast -> translate(std::cout);
-  //      std::cout << std::endl;
-  //      std::cout<<"if __name__ == \"__main__\":"<<std::endl;
-  //      std::cout<<"    import sys"<<std::endl;
-  //      std::cout<<"    ret=main()"<<std::endl;
-   //     std::cout<<"    sys.exit(ret)"<<std::endl;
-	if(std::string(argv[1]) == "--treeprint")
-	{
+    if(argv[1] == "--translate"){
+        ast -> translate(std::cout);
+        std::cout << std::endl;
+        std::cout<<"if __name__ == \"__main__\":"<<std::endl;
+        std::cout<<"    import sys"<<std::endl;
+        std::cout<<"    ret=main()"<<std::endl;
+        std::cout<<"    sys.exit(ret)"<<std::endl;
+    }
+	if(std::string(argv[1]) == "--treeprint"){
 	std::cout<<"treeprint start"<<std::endl;
 	ast->treeprint(std::cout, indent);
-      	std::cout << std::endl;
+    std::cout << std::endl;
 	}
- //   }
+ 
 	std::cout<<"finished"<<std::endl;
     return 0;
 }

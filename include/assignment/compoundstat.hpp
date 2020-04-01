38a11b39961c12c551c5943d227a2a1d9eaa90c3
Yilong <56489051+yw14218@users.noreply.github.com>
2020-04-01 04:24:33 +0100

    Add files via upload

---
+++ include/assignment/compoundstat.hpp
@@ -0,0 +1,80 @@
+#ifndef COMPOUNDSTAT_HPP
+#define COMPOUNDSTAT_HPP
+
+#include"Statement.hpp"
+#include"Statementlist.hpp"
+
+class compoundstatement :public Statement{
+public:
+	compoundstatement(){};
+	compoundstatement(Statementlistptr p1){left = p1;};
+	compoundstatement(Statementlistptr p1, Statementlistptr p2){left = p1; right = p2;};
+	virtual ~compoundstatement(){};
+	Statementlistptr get_stat1(){return left;}
+	Statementlistptr get_stat2(){return right;}
+	virtual void translate(std::ostream &dst)const override{
+		if(left != NULL)
+		{
+		for(int i=0;i<left->size();i++)
+		{
+			dst << "\t";
+			(left->at(i))->translate(dst);
+			dst << std::endl;
+		}
+		}	
+		if(right != NULL)
+		{
+		for(int i=0;i<right->size();i++)
+		{
+			dst << "\t";
+			(right->at(i))->translate(dst);
+			dst << std::endl;
+		}
+		}	
+	}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<Compound statement> ["<<'\n';
+		if(left != NULL)
+		{
+		for(int i=0;i<left->size();i++)
+		{
+			(left->at(i))->treeprint(dst, indent+"  ");
+		}
+		}	
+		if(right != NULL)
+		{
+		for(int i=0;i<right->size();i++)
+		{
+			(right->at(i))->treeprint(dst, indent+"  ");
+		}
+		}
+      		dst << indent << "]"<<'\n';
+	};
+private:
+	Statementlistptr left;
+	Statementlistptr right;
+	
+};
+
+class global : public Statement{
+public:
+	virtual ~global(){};
+	global(Statementlistptr p1){left = p1;};
+	Statementlistptr get_stat1(){return left;}
+	virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<global> ["<<'\n';
+		for(int i=0;i<left->size();i++)
+		{
+			(left->at(i))->treeprint(dst, indent+"  ");
+			dst<<'\n';
+		}
+		dst<<indent<<"]"<<'\n';
+	};
+private:
+	Statementlistptr left;
+};
+
+
+
+#endif

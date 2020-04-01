8655f8d60b85587f3bb24303ff95a647204f5d83
Yilong <56489051+yw14218@users.noreply.github.com>
2020-04-01 04:25:44 +0100

    Add files via upload

---
+++ include/jumpstatement.hpp
@@ -0,0 +1,55 @@
+#ifndef JUMPSTATEMENT_HPP
+#define JUMPSTATEMENT_HPP
+
+#include "Statement.hpp"
+#include "Statementlist.hpp"
+
+class jumpstatement : public Statement {
+public:
+	jumpstatement(){};
+	virtual ~jumpstatement(){};
+	virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<jump statement> "<<"jump"<<'\n';
+	};
+};
+
+class breakstatement : public Statement {
+public:
+	breakstatement(){};
+	virtual ~breakstatement(){};
+	virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<break statement> "<<"break"<<'\n';
+	};
+};
+
+class returnstatement : public Statement {
+public:
+	returnstatement(){};
+	returnstatement(Statementptr p1){left = p1;};
+	Statementptr get_stat(){return left;};
+	virtual ~returnstatement(){};
+	virtual void translate(std::ostream &dst)const override{
+		dst << "return ";
+		if(left!=NULL){left -> translate(dst);}	
+	}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<return statement> [ "<<'\n';
+		if(left != NULL)
+			{left->treeprint(dst, indent+"  ");}
+		dst<<indent<<"]"<<'\n';
+	};
+private:
+	Statementptr left;
+};
+class continuestatement : public Statement {
+public:
+	continuestatement(){};
+	virtual ~continuestatement(){};
+	virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"<Continue statement> continue"<<'\n';
+	};
+};
+#endif
---
+++ include/labeledstatement.hpp
@@ -0,0 +1,61 @@
+#ifndef LABELEDSTATEMENT_HPP
+#define LABELEDSTATEMENT_HPP
+
+#include "Statement.hpp" 
+#include "Statementlist.hpp"
+
+class labeledstatement: public Statement{
+public:
+	labeledstatement(Statementptr p1, Statementptr p2){left = p1; right = p2;};
+	virtual ~labeledstatement(){};
+	Statementptr get_label(){return left;};
+	Statementptr get_stat(){return right;};
+	virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"Labeled Statement> ["<<'\n';
+		left->treeprint(dst,indent+"  ");
+		right->treeprint(dst,indent+"  ");
+		dst<<indent<<"]"<<'\n';
+	};
+protected:
+	Statementptr left;
+	Statementptr right;
+
+};
+
+class caselabeledstatement : public Statement{
+public:
+	caselabeledstatement(Statementptr p1, Statementptr p2){left = p1; right = p2;};
+	virtual ~caselabeledstatement(){};
+	Statementptr get_label(){return left;};
+	Statementptr get_stat(){return right;};;
+  virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"CaseLabeled Statement> ["<<'\n';
+		left->treeprint(dst,indent+"  ");
+		right->treeprint(dst,indent+"  ");
+		dst<<indent<<"]"<<'\n';
+	};
+protected:
+	Statementptr left;
+	Statementptr right;
+};
+
+class defaultlabeledstatement : public Statement{
+public:
+	defaultlabeledstatement(Statementptr p1){left = p1;};
+	virtual ~defaultlabeledstatement(){};
+	Statementptr get_stat(){return left;};
+  virtual void translate(std::ostream &dst)const override{}
+	virtual void treeprint(std::ostream &dst, std::string indent)const override {
+		dst<<indent<<"Default Statement> ["<<'\n';
+		left->treeprint(dst,indent+"  ");
+		dst<<indent<<"]"<<'\n';
+	};
+protected:
+	Statementptr left;
+};
+
+
+
+#endif
